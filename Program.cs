3135316404bcf7bc747afafb7a3fd55cdb9a611e
carter-oleary <carter.m.oleary@outlook.com>
2025-05-05 11:25:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 1234;
+Console.WriteLine($"The sum of the digits of {n} is {SumDigits(n)}");
+
+int SumDigits(int n)
+{
+    string s = n.ToString();
+    int sum = 0;
+    foreach (char c in s)
+        sum += Convert.ToInt32(c) - 48;
+    return sum;
+}
